6101be1f93e50e2a585ca16e491410967a83a02e
Alan Voss <alanvoss@hotmail.com>
2019-12-14 20:16:22 -0600

    intent fix for day 2 part 1

--- 2019/2/part1.rb
+++ 2019/2/part1.rb
@@ -7,20 +7,20 @@ inputs[1] = 12
 inputs[2] = 2
 
 (0..(inputs.length / 4)).each do |n|
-    opcode, operand1, operand2, position = inputs[n * 4, 4]
+  opcode, operand1, operand2, position = inputs[n * 4, 4]
 
-    if opcode == 1
-        inputs[position] = inputs[operand1] + inputs[operand2]
-        puts "new #{position} is #{inputs[position]}"
-    elsif opcode == 2
-        inputs[position] = inputs[operand1] * inputs[operand2]
-        puts "new #{position} is #{inputs[position]}"
-    elsif opcode == 99
-        puts "all done"
-        break
-    else
-        raise "We shouldn't have an opcode of #{opcode}"
-    end
+  if opcode == 1
+    inputs[position] = inputs[operand1] + inputs[operand2]
+    puts "new #{position} is #{inputs[position]}"
+  elsif opcode == 2
+    inputs[position] = inputs[operand1] * inputs[operand2]
+    puts "new #{position} is #{inputs[position]}"
+  elsif opcode == 99
+    puts "all done"
+    break
+  else
+    raise "We shouldn't have an opcode of #{opcode}"
+  end
 end
 
 puts inputs[0]
